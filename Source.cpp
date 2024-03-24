7acfc71ce1603d134bfd29a97a63d04bcccfa4da
CeBacT9lH <164763729+CeBacT9lH@users.noreply.github.com>
2024-03-24 17:59:45 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL,"ru");
+	double x, y;
+	char a;
+
+	cin >> x >> y >> a;
+
+	switch (a)
+	{
+	case '+':
+			cout << x + y;
+		break;
+
+	case '-':
+			cout << x - y;
+		break;
+
+	case '*':
+			cout << x * y;
+		break;
+
+	case '/':
+			cout << x / y;
+		break;
+	default:
+		cout << "Такой операции нет!";
+		break;
+	}
+}
